73d2611fd9cb9f52685f3e792696e451ad51fc6d
bigBadMatt <5707@holbertonstudents.com>
2022-12-12 21:27:30 -0800

    added psudo code

--- main.c
+++ main.c
@@ -4,10 +4,19 @@
  *
  *
  *
-*/k
-int main()
+*/
+int main(int argc, char *argv[])
 {
-	//read from text file
+	//check to make sure argv[1] is a path to a file
+	//fopen file
+	//error if file is not there
+	//While loop:
+        //    getline from text file
+	//    strtok the getline
+	//    call corresonding function according to the first word
+	//    pass 2nd word as arg
+	//    return
+
 	
 	return (0);
 }
