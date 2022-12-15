a1a0337caaa64db6ac751927419ccf2a006b84db
bigBadMatt <5707@holbertonstudents.com>
2022-12-14 20:39:46 -0800

    fixed edgecase checkcauseing a segfault

--- swap.c
+++ swap.c
@@ -4,12 +4,12 @@ void opcode_swap(stack_t **stack, unsigned int line_number)
 {
 	int tmp;
 
-	if((* stack)->next == NULL || stack == NULL)
+	if((* stack) == NULL || stack == NULL || (* stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	
 	tmp = (* stack)->n;
 	(* stack)->n = ((* stack)->next)->n;
 	((* stack)->next)->n = tmp;
