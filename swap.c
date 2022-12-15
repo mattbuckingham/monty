385e412129ef5ab2492645d12c6488d779b139c4
bigBadMatt <5707@holbertonstudents.com>
2022-12-14 20:28:32 -0800

    added check for case stack is non-existent

--- swap.c
+++ swap.c
@@ -4,7 +4,7 @@ void opcode_swap(stack_t **stack, unsigned int line_number)
 {
 	int tmp;
 
-	if((*stack)->next == NULL)
+	if((* stack)->next == NULL || stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
