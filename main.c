2bacb6ad665263e4ca5800fc5dfafe839becbc1a
pallavi149 <5811@holbertonstudents.com>
2022-12-14 19:28:02 -0800

    Added pint function

--- main.c
+++ main.c
@@ -62,15 +62,12 @@ void find_opcode(char *command, stack_t **stack, unsigned int line_number)
 	instruction_t op[] = {
 		{"push", opcode_push},
 		{"pall", opcode_pall},
-/*
- *		{"pint", opcode_pint},
- *		{"pop", opcode_pop},
+		{"pint", opcode_pint},
+/*		{"pop", opcode_pop},
  *		{"swap", opcode_swap},
  *		{"add", opcode_add},
  *		{"nop", opcode_nop},
 */
-
-
 		{NULL, NULL}
 	};
 	len = strlen(command);
