ec92346b33128ee9879c97de776fcb82436f5892
pallavi149 <5811@holbertonstudents.com>
2022-12-14 19:11:15 -0800

    Adding pint function

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * opcode_pint - prints the value at the top of the stack,
+ * followed by a new line
+ * @stack: pointer to top of stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void opcode_pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
